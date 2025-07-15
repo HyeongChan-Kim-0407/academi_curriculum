ce8fe7cca3ae00a08f59e4646bece79c263875f3
Sku-Kim <khc_0407@naver.com>
2025-07-15 07:41:55 +0000

    2025-07-15 스프링 부트 BoardProject를 위한 DB 계정 및 테이블 생성 확인 쿼리문

---
+++ sql/BOOT_KHC.sql
@@ -0,0 +1,12 @@
+-- SYS 계정으로 접속
+-- 공통사용자 규칙 해제
+ALTER SESSION SET "_ORACLE_SCRIPT" = TRUE;
+-- LOCAL DB에 계정 생성
+CREATE USER BOOT_KHC IDENTIFIED BY "1234";
+GRANT DBA TO BOOT_KHC; -- 관리자 권한 부여
+
+-- BOOT_KHC 계정으로 접속
+
+desc testboard;
+
+select * from testboard;
